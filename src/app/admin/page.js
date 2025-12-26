f3c94b69a2c255a9dd7903ea4dcfa800344610d4
Sadanand Kumar Yadav <sadanandsidhu@gmail.com>
2025-12-26 23:02:18 +0530

    s

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
+'use client';
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
-'use client';
 import { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
